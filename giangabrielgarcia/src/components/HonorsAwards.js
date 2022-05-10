c0ff7e370748f040f6ab1279639eab3b62b27801
Stephanie Fajardo <stfajardo@gmail.com>
2022-05-10 14:34:03 -0400

    add awards and honors

--- giangabrielgarcia/src/components/HonorsAwards.js
+++ giangabrielgarcia/src/components/HonorsAwards.js
@@ -2,10 +2,27 @@ const HonorsAwards = () => {
     return (
         <div className='body-container'> 
             <div className='hide-body'>
-                <h1>
-                    Honors and awards page
-                </h1>
-            </div>
+                <h3 className='page-title center'>Awards and Honors</h3>
+                    <ul>
+                        <li> <b>Panel Fellow, National Science Foundation CMMI Game Changers Academy (2021-2022) </b> </li>
+                        <li> <b>Best Paper, IISE Transactions Focus Issue on Operations Engineering and Analytics (2021) </b> </li>
+                        <li> <b>First Prize, Society of Medical Decision Making Lee B. Lusted Prize in <i>QMTD</i> (2020)</b> </li>
+                        <li> <b>First Prize, INFORMS Minority Issues Forum Paper Competition (2020)</b>  </li>
+                        <li> <b>Richard F. and Eleanor A. Towner Prize for Distinguished Academic Achievement (2020) </b> </li>
+                        <li> Departmental Nomination, Cornell ORIE Young Researchers Workshop (2019) </li>
+                        <li> <b>First Prize, Society of Medical Decision Making Lee B. Lusted Prize in <i>QMTD</i> (2018) </b> </li>
+                        <li> <b>INFORMS Bonder Scholarship for Applied OR In Health Services (2018) </b> </li>
+                        <li> Departmental Nomination, INFORMS Doctoral Colloquium (2018) </li> 
+                        <li> <b>First Prize, INFORMS Minority Issues Forum Poster Competition (2017)</b> </li>
+                        <li> Finalist, INFORMS Bonder Scholarship for Applied OR In Health Services (2017) </li>
+                        <li> IOE GSI of the Semester (Winter 2017) </li>
+                        <li> <b>National Science Foundation Graduate Research Fellowship (2017-2020)</b> </li>
+                        <li> Honorable Mention, IOE Bonder Scholar Fellowship (2016) </li>
+                        <li> Honorable Mention, Ford Foundation Fellowship Predoctoral Competition (2016, 2017) </li>
+                        <li> Rackham Travel Grant (Winter 2016, Fall 2016) </li>
+                        <li> Rackham Merit Fellowships (2015 - 2019) </li>
+                    </ul>
+                </div>
         </div>
     )
 }
