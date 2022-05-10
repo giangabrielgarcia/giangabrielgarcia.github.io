f2de6ee77ff0128871a1b605cec63bd593529a7e
Stephanie Fajardo <stfajardo@gmail.com>
2022-05-10 16:50:36 -0400

    update diversity page

--- giangabrielgarcia/src/components/Diversity.js
+++ giangabrielgarcia/src/components/Diversity.js
@@ -4,12 +4,32 @@ const Diversity = () => {
     return (
         <div className='body-container'> 
             <div className='hide-body'>
-                <h1>
-                    Diversity page
-                </h1>
-            </div>
-       
+                <div className="callout">
+                    <h4>Diversity</h4>
+                    <p><u>Definition</u>:<i> (noun) the inclusion of individuals representing more than one national origin, color, religion, socioeconomic stratum, sexual orientation, etc.</i>
+                    <small><br></br><a className='reg-link' href="http://dictionary.reference.com/browse/diversity?s=t">Dictionary.com</a></small></p>
+	            </div>
+                <div className='mb-50'>
+                    <p>As an undergraduate student at the University of Pittsburgh, I was fortunate enough to take part in numerous organizations that have helped me to foster a deep appreciation for the 
+                    power of diversity, equity, and inclusion in sciences, technology, engineering, and mathematics (STEM). Since that time,
+                    I have made and will continue to make efforts to increase diversity in STEM by taking part in diversity initiatives, mentoring, and outreach. 
+                    </p>
+                </div>
+	            <div className = "row">
+                    <h3> Diversity Links </h3>
+                    <p>The organizations listed below have helped me to grow personally and professionally. I am very grateful for the support and opportunities they have presented to me:</p>
+                    <ul className='diversity-links'>
+                        <li><a className='reg-link' href="http://femmes.studentorgs.umich.edu/">Females Exceling More in Mathematics, Engineering, and Science (FEMMES)</a></li>
+                        <li> IOE Diversity Initiative </li>
+                        <li> <a className='reg-link' href="http://www.pittbrotherhood.com"> B.R.O.T.H.E.R.H.O.O.D. </a></li>
+                        <li><a className='reg-link' href="http://www.engineering.pitt.edu/Student/Student-Programs/Excel/">Pitt EXCEL</a></li>
+                        <li><a className='reg-link' href="http://www.famefund.org/programs/fameacademy/">FAME Academy </a></li>
+                        <li><a className='reg-link' href="http://www.engineering.pitt.edu/SHPE/">Pitt SHPE</a></li>
+                        <li><a className='reg-link' href="http://www.pittnsbe.org/">Pitt NSBE</a></li>
+                    </ul>
+	            </div>
         </div>
+    </div>
     )
 }
 
--- giangabrielgarcia/src/components/HonorsAwards.js
+++ giangabrielgarcia/src/components/HonorsAwards.js
@@ -2,29 +2,34 @@ const HonorsAwards = () => {
     return (
         <div className='body-container'> 
             <div className='hide-body'>
-                <h3 className='page-title center'>Awards and Honors</h3>
-                    <ul>
-                        <li> <b>Panel Fellow, National Science Foundation CMMI Game Changers Academy (2021-2022) </b> </li>
-                        <li> <b>Best Paper, IISE Transactions Focus Issue on Operations Engineering and Analytics (2021) </b> </li>
-                        <li> <b>First Prize, Society of Medical Decision Making Lee B. Lusted Prize in <i>QMTD</i> (2020)</b> </li>
-                        <li> <b>First Prize, INFORMS Minority Issues Forum Paper Competition (2020)</b>  </li>
-                        <li> <b>Richard F. and Eleanor A. Towner Prize for Distinguished Academic Achievement (2020) </b> </li>
-                        <li> Departmental Nomination, Cornell ORIE Young Researchers Workshop (2019) </li>
-                        <li> <b>First Prize, Society of Medical Decision Making Lee B. Lusted Prize in <i>QMTD</i> (2018) </b> </li>
-                        <li> <b>INFORMS Bonder Scholarship for Applied OR In Health Services (2018) </b> </li>
-                        <li> Departmental Nomination, INFORMS Doctoral Colloquium (2018) </li> 
-                        <li> <b>First Prize, INFORMS Minority Issues Forum Poster Competition (2017)</b> </li>
-                        <li> Finalist, INFORMS Bonder Scholarship for Applied OR In Health Services (2017) </li>
-                        <li> IOE GSI of the Semester (Winter 2017) </li>
-                        <li> <b>National Science Foundation Graduate Research Fellowship (2017-2020)</b> </li>
-                        <li> Honorable Mention, IOE Bonder Scholar Fellowship (2016) </li>
-                        <li> Honorable Mention, Ford Foundation Fellowship Predoctoral Competition (2016, 2017) </li>
-                        <li> Rackham Travel Grant (Winter 2016, Fall 2016) </li>
-                        <li> Rackham Merit Fellowships (2015 - 2019) </li>
+                {/* <h3 className='page-title'>National and International Prizes</h3> */}
+                    <div className='awards-container'>
+                    <h3 className='page-title'>National and International Awards</h3>
+                    <hr></hr>
+                    <ul className='ms-0 ps-0 mb-50'>
+                        <li className='break'>Panel Fellow, National Science Foundation CMMI Game Changers Academy (2021-2022)</li>
+                        <li className='break'><i className='bi-award h5'/> &nbsp; Best Paper, IISE Transactions Focus Issue on Operations Engineering and Analytics (2021)</li>
+                        <li className='break'><i className='bi-award h5'/> &nbsp; First Prize, Society of Medical Decision Making Lee B. Lusted Prize in <i>QMTD</i> (2020)</li>
+                        <li className='break'><i className='bi-award h5'/> &nbsp; First Prize, INFORMS Minority Issues Forum Paper Competition (2020)</li>
+                        <li className='break'><i className='bi-award h5'/> &nbsp; First Prize, Society of Medical Decision Making Lee B. Lusted Prize in <i>QMTD</i> (2018)</li>
+                        <li className='break'>INFORMS Bonder Scholarship for Applied OR In Health Services (2018)</li>
+                        <li className='break'><i className='bi-award h5'/> &nbsp; First Prize, INFORMS Minority Issues Forum Poster Competition (2017)</li>
+                        <li className='break'>Finalist, INFORMS Bonder Scholarship for Applied OR In Health Services (2017)</li>
+                        <li className='break'>National Science Foundation Graduate Research Fellowship (2017-2020)</li>
+                        <li className='break'>Honorable Mention, Ford Foundation Fellowship Predoctoral Competition (2016, 2017)</li>
                     </ul>
+                    <h3 className='page-title'>Selected Institutional Awards</h3>
+                    <hr></hr>
+                    <ul className='ms-0 ps-0'>
+                        <li className='break'>Richard F. and Eleanor A. Towner Prize for Distinguished Academic Achievement (2020)</li>
+                        <li className='break'>IOE GSI of the Semester (Winter 2017)</li>
+                        <li className='break'>Rackham Merit Fellowships (2015 - 2019)</li>
+                    </ul>
+                    </div>
                 </div>
         </div>
     )
 }
 
 export default HonorsAwards; 
+
